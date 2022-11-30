5f420d4605002d1c7f584e1a2efe94eb223f2a16
Jefri Tambunan <73419332+jefritambunan11@users.noreply.github.com>
2022-11-30 15:13:12 +0700

    Menambahkan file koneksi.js

---
+++ koneksi.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+
+//
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "db_restapi_ipungdev",
+});
+
+conn.connect((err) => {
+    if (err) throw err;
+    console.log("Terhubung ke Mysql");
+});
+
+module.exports = conn;
--- readME.txt
+++ readME.txt
@@ -1,2 +1,12 @@
 # install
 yarn add express mysql body-parser
+
+# buat file server.js
+touch server.js
+
+# jalankan server
+node server.js
+
+
+# buat koneksi.js
+touch koneksi.js
