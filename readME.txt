43f1e2c404ddc617adbd691ff9005ead2831b7d8
Jefri Tambunan <73419332+jefritambunan11@users.noreply.github.com>
2022-11-30 15:24:48 +0700

    Membuat file router.js

--- readME.txt
+++ readME.txt
@@ -6,6 +6,7 @@ touch server.js
 touch koneksi.js
 touch response.js
 touch controller.js
+touch router.js
 
 
 # jalankan server
---
+++ router.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+module.exports = function(app) {
+    let controller = require("./controller");
+
+    app.route("/").get(controller.index());
+}
